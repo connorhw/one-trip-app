02c4ffb8539d9bbdd0f37fa115642f3bfd8f1bf3
connorhw <connorwong93@gmail.com>
2021-01-10 21:11:13 -0800

    3rd times the charm for 1st static app submit

--- src/components/NewTrip/NewTrip.js
+++ src/components/NewTrip/NewTrip.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './NewTrip.css'
-import ImageUploader from 'react-images-upload';
 
 function NewTrip() {
 
@@ -11,14 +10,14 @@ function NewTrip() {
             <label htmlFor='places_of_interest'>Places of Interest: </label><br />
             <textarea id='places' name='places' type='text' cols="40" rows="5" required /><br />
             <label htmlFor='fav-part'>Favorite Part: </label>
-            <input type='text' id='fav-part' name='fav-part' required/><br />
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={console.log('it works')} //{this.onDrop}
-                //imgExtension={['.jpg ', '.gif ', '.png ', '.gif']}
-                maxFileSize={5242880}
-            />
+            <input type='text' id='fav-part' name='fav-part' required/><br /><br />
+            <label>Add photos to trip: </label><br />
+            <input/><button>upload</button><br />
+            <input/><button>upload</button><br />
+            <input/><button>upload</button><br />
+            
+            
+
         </form>
     )
 }
