fcc7f5b2a001245848b02610c1ca2a97abfa82ef
connorhw <connorwong93@gmail.com>
2021-03-04 22:49:33 -0500

    outlining home page to figure out a significance for it.. right now just tells user what the app is for.

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,14 +6,14 @@ function Landing() {
     return (
       
             <div className='landing'>
-                <div>
+                {/*<div>
                 <h3>Document each trip for free. Treat this application as a travel journal.</h3>
                     <ul>
                         <li>The 'Vault' is your personal repository, containing all your recorded trips</li>
                         <li>'New Plant' allows you to create new trips that you can access from your vault</li>
                         <li>You're able to edit or delete any of the trips</li>
-                    </ul>           
-                </div><br /><br />
+                </ul>        
+                </div><br /><br />*/}   
                 <label>Have an account?</label>
                 <Link to={`/SignIn`} className='form-link'>Sign In</Link>
                 <label>Want an account?</label>
