aa60c861c94805d334e072e044d82a1d49156a68
connorhw <connorwong93@gmail.com>
2021-03-09 23:46:21 -0500

    adds to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,8 @@ import './Home.css'
 function Home() {
     return (
         <div className='home'>
-            <h3>Document each trip for free. Treat this application as a travel journal.</h3>    
+            <h3>Document each trip for free. Treat this application as a travel journal.</h3>
+            <div>Submit, Edit, Delete trips once you register and sign-in for an account.</div>    
         </div>
     )
 }
